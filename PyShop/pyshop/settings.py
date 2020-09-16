c39d7cbc2b53bc2ea2af91790d235a5d3adf1933
RithanyaRajKP <70689964+RithanyaRajKP@users.noreply.github.com>
2020-09-16 14:47:03 +0530

    Update settings.py

--- PyShop/pyshop/settings.py
+++ PyShop/pyshop/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')#mb5lv^jxrjqrgm$r*6b9s0ymp*b$*llwg$qzr2)ycd_k+h68'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
